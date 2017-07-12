fa2df1b326c430879470e34f97d994e19fad45e4
nvkhoi <xinanhgiutrontinhque@gmail.com>
2017-07-12 09:38:19 +0700

    Initial commit

---
+++ stdc++.cpp
@@ -0,0 +1,6 @@
+//
+// Created by nvkhoi on 6/18/17.
+//
+
+#include "stdc++.h"
+
---
+++ stdc++.h
@@ -0,0 +1,39 @@
+//
+// Created by nvkhoi on 6/18/17.
+//
+
+#ifndef GRAPHCUTSEGMENTATION_STDC_H_H
+#define GRAPHCUTSEGMENTATION_STDC_H_H
+
+// standard template library
+
+#include <cstdio>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+
+#include <vector>
+#include <array>
+#include <list>
+#include <stack>
+#include <queue>
+#include <string>
+
+#include <algorithm>
+#include <utility>
+#include <cstdint>
+#include <cstdlib>
+
+#include <thread>
+#include <mutex>
+#include <memory>
+
+// OpenCV framework
+#include <opencv2/opencv.hpp>
+
+// Boost framework
+
+
+// User-defined
+
+#endif //GRAPHCUTSEGMENTATION_STDC_H_H
